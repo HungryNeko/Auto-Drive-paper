0d4719c395430d7b94850f91ebc0847e6de6709f
Hungry <redstonedust0117@gmail.com>
2024-02-22 18:46:17 +0800

    update show
    add function to show all roads

--- show.py
+++ show.py
@@ -2,84 +2,103 @@ import numpy as np
 import matplotlib.pyplot as plt
 from shapely.geometry import Point, LineString
 from pyproj import CRS, Transformer
-from read import readtxt, dict_data
+from read import readtxt, dict_data  # Assuming readtxt and dict_data functions are defined in the 'read' module
 from multiprocessing import Pool
 from tqdm import tqdm
 from functools import partial
+from shapely.ops import transform
+from functools import partial
+import pyproj
+import os
 
-def is_vehicle_on_road(vehicle_coords, road_coords, lane_width=11.25):
-    transformer = Transformer.from_crs(CRS('EPSG:4326'), CRS('EPSG:3857'), always_xy=True)
-    vehicle_coords_proj = transformer.transform(*vehicle_coords)
-    road_coords_proj = [transformer.transform(*road_coord) for road_coord in road_coords]
-    vehicle_point = Point(vehicle_coords_proj)
-    road_line = LineString(road_coords_proj)
-    distance = vehicle_point.distance(road_line)
-    return distance <= lane_width
-
-def process_data(car, road_coords):
-    if is_vehicle_on_road((car['lon'], car['lat']), road_coords):
-        return car
-
-def plot_on_road_points(ax, on_road_data):
-    unique_car_ids = set(car['car_id'] for car in on_road_data)
-    colors = plt.cm.rainbow(np.linspace(0, 1, len(unique_car_ids)))  # 生成彩虹颜色
-    color_dict = dict(zip(unique_car_ids, colors))  # 创建一个字典，将车辆ID和颜色对应起来
-    added_labels = set()  # 用于存储已经添加过的图例
-    for car in on_road_data:
-        lon, lat = car['lon'], car['lat']
-        car_id = car['car_id']
-        color = color_dict[car_id]  # 根据车辆ID获取颜色
-        label = f"Car {car_id}" if car_id not in added_labels else ""  # 如果已经添加过图例，则不再添加
-        ax.scatter(lon, lat, marker='o', color=color, label=label)
-        added_labels.add(car_id)  # 将车辆ID添加到已添加的图例中
-
-    ax.legend()
+def plot_road(trajectory, width=22.5, style='-', color='black'):
+    line = LineString(trajectory)
+    # 创建一个转换器，将WGS84坐标转换为UTM坐标
+    transformer = pyproj.Transformer.from_crs('epsg:4326', 'epsg:32650', always_xy=True)
+    line_transformed = transform(transformer.transform, line)
+    # 创建道路的两侧线
+    left_line = line_transformed.parallel_offset(width / 2, 'left')
+    right_line = line_transformed.parallel_offset(width / 2, 'right')
+    # 将UTM坐标转换回WGS84坐标
+    transformer = pyproj.Transformer.from_crs('epsg:32650', 'epsg:4326', always_xy=True)
+    left_line = transform(transformer.transform, left_line)
+    right_line = transform(transformer.transform, right_line)
+    # 绘制道路的两侧线
+    ax.plot(*left_line.xy, style, color=color)
+    ax.plot(*right_line.xy, style, color=color)
 
 
-if __name__ == "__main__":
-    file = './路网数据/北三环东路.txt'
-    with open(file, 'r') as f:
-        road_coords = eval(f.read())
+def plot_trajectories(trajectories):
+    for trajectory in trajectories:
+        coords = np.array(trajectory)
+        ax.plot(coords[:, 0], coords[:, 1], '--', color='gray')  # 绘制中心线
+        plot_road(coords, width=22.5, style='-', color='black')  # 绘制道路
 
-    road_coords = [coord for sublist in road_coords for coord in sublist]
+# if __name__ == "__main__":
+#     file = './路网数据/S50北五环.txt'
+#     with open(file, 'r') as f:
+#         data = eval(f.read())
+#
+#     #readtxt('log.txt')
+#
+#     fig, ax = plt.subplots(figsize=(10, 8))
+#
+#     # 绘制道路
+#     plot_trajectories(data)
+#
+#     plt.show()
 
-    readtxt('E:/Studyproject/re/log.txt')#完整的log
 
-    # 用于存储符合条件的数据
-    filtered_data = []
+def plot_multiple_trajectories(directory):
+    global  fig, ax
+    # 遍历指定目录下的所有txt文件
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):
+            # 创建新的图形对象和Axes对象
+            fig, ax = plt.subplots(figsize=(10, 8))
+            xlim = None
+            ylim = None
 
-    with Pool() as pool:
-        for car_id, car_list in tqdm(dict_data.items(), desc='Processing Data'):
-            for car in car_list:
-                car['car_id'] = car_id  # 添加车辆ID字段
-                if is_vehicle_on_road((car['lon'], car['lat']), road_coords):
-                    # 符合条件的数据，将其加入 filtered_data
-                    filtered_data.append(car)
+            filepath = os.path.join(directory, filename)
+            with open(filepath, 'r') as f:
+                data = eval(f.read())
 
-    # 使用进程处理数据
-    with Pool() as pool:
-        process_data_partial = partial(process_data, road_coords=road_coords)
-        on_road_data = list(tqdm(pool.imap(process_data_partial, filtered_data), desc='Processing Data', total=len(filtered_data)))
+            # 找到所有轨迹中的最小和最大横纵坐标
+            for trajectory in data:
+                coords = np.array(trajectory)
+                traj_xlim = [np.min(coords[:, 0]), np.max(coords[:, 0])]
+                traj_ylim = [np.min(coords[:, 1]), np.max(coords[:, 1])]
 
-    # 绘制在路上的点
-    fig, ax = plt.subplots(figsize=(10, 8))
+                # 更新整体的横纵坐标范围
+                if xlim is None:
+                    xlim = traj_xlim
+                else:
+                    xlim = [min(xlim[0], traj_xlim[0]), max(xlim[1], traj_xlim[1])]
 
-    # 绘制道路
-    road_coords = np.array(road_coords)
-    ax.plot(road_coords[:, 0], road_coords[:, 1], color='black', label='Road')
+                if ylim is None:
+                    ylim = traj_ylim
+                else:
+                    ylim = [min(ylim[0], traj_ylim[0]), max(ylim[1], traj_ylim[1])]
 
-    # 绘制在路上的点
-    plot_on_road_points(ax, on_road_data)
+            # 计算纵横比例
+            x_length = xlim[1] - xlim[0]
+            y_length = ylim[1] - ylim[0]
+            aspect_ratio = y_length / x_length
 
-    # 设置图例
-    ax.legend().set_visible(False)
+            # 设置横纵坐标范围
+            ax.set_xlim(xlim)
+            ax.set_ylim(ylim)
 
-    # 输出点在这条路上的总数
-    num_cars = len(dict_data)
-    total_points_on_road = len(on_road_data)
-    num_cars_on_road = len(set(car['car_id'] for car in on_road_data))  # 计算在路上的车辆数量
-    print(f"Number of cars in the dataset: {num_cars}")
-    print(f"Total points on the road: {total_points_on_road}")
-    print(f"Number of cars on the road: {num_cars_on_road}")  # 输出在路上的车辆数量
+            # 设置纵横比例
+            ax.set_aspect(aspect_ratio)
 
-    plt.show()
+            # 绘制道路
+            plot_trajectories(data)
+
+            # 显示图形
+            plt.show()
+
+
+# 使用示例
+directory = './路网数据/'
+plot_multiple_trajectories(directory)
