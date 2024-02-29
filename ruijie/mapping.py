a97c9fd294cfc6864065c46d497e08f4cc6a6e8d
Hungry <redstonedust0117@gmail.com>
2024-02-29 13:27:54 +0800

    Revert "Update mapping.py"
    This reverts commit 41eb345584749c12b5af559dd44f3339004f3503.

--- ruijie/mapping.py
+++ ruijie/mapping.py
@@ -1,67 +1,46 @@
-import pyproj
 import numpy as np
-from tqdm import tqdm
-import seaborn as sns
 import matplotlib.pyplot as plt
-from multiprocessing import Pool
-from shapely.ops import transform
+import seaborn as sns
+from shapely.geometry import Point, LineString
+from pyproj import CRS, Transformer
 from read import readtxt, dict_data  # Assuming readtxt and dict_data functions are defined in the 'read' module
-from shapely.geometry import Point, LineString, Polygon
-
-def create_road_polygon(trajectory):
-    line = LineString(trajectory)
-    transformer = pyproj.Transformer.from_crs('epsg:4490', 'epsg:32650', always_xy=True)
-    line_transformed = transform(transformer.transform, line)
-    left_line = line_transformed.parallel_offset(11.25, 'left')
-    right_line = line_transformed.parallel_offset(11.25, 'right')
-    transformer = pyproj.Transformer.from_crs('epsg:32650', 'epsg:4490', always_xy=True)
-    left_line = transform(transformer.transform, left_line)
-    right_line = transform(transformer.transform, right_line)
-    road_polygon = Polygon(np.concatenate([left_line.xy, np.fliplr(right_line.xy)], axis=1).T)
-    return road_polygon
+from multiprocessing import Pool
+from tqdm import tqdm
+from functools import partial
+from shapely.ops import transform
+from functools import partial
+import pyproj
+import matplotlib.cm as cm
+import matplotlib.patches as mpatches
 
 def check_point_on_road(args):
-    point_data, road_polygons = args
+    point_data, trajectories, i = args
     point = Point(point_data['lon'], point_data['lat'])
-    for road_polygon in road_polygons:
-        if road_polygon.contains(point):
-            return (point_data['speed'], point_data['acceleration'], point_data['angle'])
+    for trajectory in trajectories:
+        road = LineString(trajectory)
+        if road.buffer(0.0002).contains(point):  # Adjust the buffer size as needed
+            return (point_data['lon'], point_data['lat'], str(point_data['time']), i)
     return None
 
-def plot_trajectories(trajectories, dict_data):
-    args = []
-    speed_data = []
-    acceleration_data = []
-    angle_data = []
-    for car_data in dict_data.values():
-        for point_data in car_data:
-            args.append((point_data, trajectories))
-
-    with Pool() as p:
-        for result in tqdm(p.imap(check_point_on_road, args), total=len(args)):
-            if result is not None:
-                speed, acceleration, angle = result
-                speed_data.append(speed)
-                acceleration_data.append(acceleration)
-                angle_data.append(angle)
-
-    fig, axs = plt.subplots(3, 1, figsize=(10, 24))  # 创建一个3行1列的图形数组
-
-    sns.histplot(speed_data, bins=30, kde=True, ax=axs[0])  # 在第一个子图上绘制速度数据
-    sns.histplot(acceleration_data, bins=30, kde=True, ax=axs[1])  # 在第二个子图上绘制加速度数据
-    sns.histplot(angle_data, bins=30, kde=True, ax=axs[2])  # 在第三个子图上绘制角度数据
-    axs[0].set_title('speed distribution')
-    axs[1].set_title('acceleration distribution')
-    axs[2].set_title('angle distribution')
+def plot_distribution(dict_data, fields):
+    fig, axs = plt.subplots(len(fields), 1, figsize=(10, 8))
+    for ax, field in zip(axs, fields):
+        data = []
+        for car_id, car_data in tqdm(dict_data.items(), desc=f'Processing {field}'):
+            for point_data in car_data:
+                data.append(point_data[field])
+        sns.histplot(data, bins=30, kde=True, ax=ax)
+        ax.set_title(f'Distribution of {field}')
+        ax.set_xlabel(field)
+        ax.set_ylabel('Frequency')
+    plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
-    file = './路网数据/西二环.txt'
+    file = './路网数据/东二环.txt'
     with open(file, 'r') as f:
         data = eval(f.read())
 
     readtxt('log.txt')
 
-    road_polygons = [create_road_polygon(trajectory) for trajectory in data]
-
-    plot_trajectories(road_polygons, dict_data)
+    plot_distribution(dict_data, ['speed', 'acceleration', 'angle'])
